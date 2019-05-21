4692ecada0501946c9c630aadceff3c7671186f6
MSI <MSI@172.17.101.157>
2019-05-21 15:45:29 +0800

    new

---
+++ CAproject/src/main/java/com/example/demo/CAprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CAprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CAprojectApplication.class, args);
+	}
+
+}
